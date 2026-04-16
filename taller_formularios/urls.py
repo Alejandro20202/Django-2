edb09976f5e4aaf8d9904090f24eea7ddcc1819f
EstebanR26 <david.ordonezren@campusucc.edu.co>
2026-04-16 13:43:50 -0500

    Revert "fix: redirigir / a /solicitudes/crear"
    This reverts commit 0ff87e9e458b10f0cf989536f448a5a6192137ec.

--- taller_formularios/urls.py
+++ taller_formularios/urls.py
@@ -18,10 +18,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/solicitudes/crear/', permanent=False)),
     path('admin/', admin.site.urls),
     path('asistencia/', include('asistencia.urls')),
     path('solicitudes/', include('solicitudes.urls')),
