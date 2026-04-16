03e1f32ead93eccf81fbb330871f05e441163dc8
EstebanR26 <david.ordonezren@campusucc.edu.co>
2026-04-16 13:20:03 -0500

    fix: redirigir / a /solicitudes/crear

--- taller_formularios/urls.py
+++ taller_formularios/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/solicitudes/crear/', permanent=False)),
     path('admin/', admin.site.urls),
     path('asistencia/', include('asistencia.urls')),
     path('solicitudes/', include('solicitudes.urls')),
